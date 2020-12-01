4906e080b344948664940dae2d3c3efcf3efbc1b
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-01 08:59:24 +0100

    Initial Commit

---
+++ Advent/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Advent
+//
+//  Created by Wilhelm Thieme on 01/12/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Advent/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Advent
+//
+//  Created by Wilhelm Thieme on 01/12/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
