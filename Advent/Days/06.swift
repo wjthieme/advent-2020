88bf5cfbc6cf1b32438b04c061b0dd09152e6d32
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-06 09:13:25 +0100

    Day 6

--- Advent/Days/06.swift
+++ Advent/Days/06.swift
@@ -9,12 +9,23 @@ import Foundation
 
 extension Solvers {
     @objc static let day6a: Solve = { input in
-
-        return "0"
+        let values = processInput(input).map { $0.flatMap { $0 } }
+        let uniques = values.map { Set($0) }
+        let counts = uniques.map { $0.count }
+        let sum = counts.reduce(0, +)
+        return "\(sum)"
     }
     
     @objc static let day6b: Solve = { input in
-
-        return "0"
+        let values = processInput(input)
+        let intersections = values.map  { $0.reduce(into: Set($0.first ?? []), { $0 = $0.intersection($1) }) }
+        let counts = intersections.map { $0.count }
+        let sum = counts.reduce(0, +)
+        return "\(sum)"
     }
+}
+
+fileprivate func processInput(_ string: String) -> [[Set<Character>]] {
+    let lines = string.components(separatedBy: "\n\n")
+    return lines.map { $0.components(separatedBy: "\n").map { Set($0) } }
 }
--- Tests/Days/06.swift
+++ Tests/Days/06.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day6a = ""
+    @objc static let day6a = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"
     @objc static let day6b = day6a
 }
 
 extension Outputs {
-    @objc static let day6a = "0"
-    @objc static let day6b = "0"
+    @objc static let day6a = "11"
+    @objc static let day6b = "6"
 }
