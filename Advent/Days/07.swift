762cbe2687a08a19d77cdb58feb270f7c2b12e87
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-07 09:02:33 +0100

    Day 7

--- Advent/Days/07.swift
+++ Advent/Days/07.swift
@@ -9,12 +9,67 @@ import Foundation
 
 extension Solvers {
     @objc static let day7a: Solve = { input in
-
-        return "0"
+        let rules = Rules(input)
+        let options = mayContain(rules, bag: "shiny gold")
+        return "\(options.count)"
     }
     
     @objc static let day7b: Solve = { input in
-
-        return "0"
+        let rules = Rules(input)
+        let options = bagsInside(rules, bag: "shiny gold")
+        return "\(options)"
     }
+}
+
+fileprivate func mayContain(_ rules: Rules, bag: String) -> Set<String> {
+    let mayContainDirectly = Set(rules.filter { $0.value[bag] != nil }.keys)
+    let mayContainIndirectly = mayContainDirectly.map { mayContain(rules, bag: $0) }
+    return mayContainIndirectly.reduce(into: mayContainDirectly) {  $0 = $0.union($1) }
+}
+
+fileprivate func bagsInside(_ rules: Rules, bag: String) -> Int {
+    guard let rule = rules[bag] else { return 0 }
+    let direct = rule.map({ $0.value }).reduce(0, +)
+    let indirect = rule.map { bagsInside(rules, bag: $0.key) * $0.value }
+    return indirect.reduce(direct, +)
+}
+
+fileprivate typealias Rules = [String: Subrules]
+fileprivate typealias Subrules = [String: Int]
+
+fileprivate extension Rules {
+    static let regex = try! NSRegularExpression(pattern: "^(\\w+ \\w+) bags contain (.+).$", options: [.caseInsensitive, .anchorsMatchLines])
+    
+    init(_ string: String) {
+        let range = NSRange(location: 0, length: string.count)
+        let matches = Rules.regex.matches(in: string, options: [], range: range)
+        
+        self = [:]
+        for match in matches {
+            guard let colorRange = Range(match.range(at: 1), in: string) else { continue }
+            let color = String(string[colorRange])
+            guard let subrulesRange = Range(match.range(at: 2), in: string) else { continue }
+            let subrules = Subrules(String(string[subrulesRange]).replacingOccurrences(of: ", ", with: "\n"))
+            self[color] = subrules
+        }
+    }
+}
+
+fileprivate extension Subrules {
+    static let regex = try! NSRegularExpression(pattern: "^(\\d+) (\\w+ \\w+) bags?$", options: [.caseInsensitive, .anchorsMatchLines])
+    
+    init(_ string: String) {
+        let range = NSRange(location: 0, length: string.count)
+        let matches = Subrules.regex.matches(in: string, options: [], range: range)
+        
+        self = [:]
+        for match in matches {
+            guard let quantityRange = Range(match.range(at: 1), in: string) else { continue }
+            let quantity = Int(string[quantityRange]) ?? 0
+            guard let colorRange = Range(match.range(at: 2), in: string) else { continue }
+            let color = String(string[colorRange])
+            self[color] = quantity
+        }
+    }
+    
 }
--- Tests/Days/07.swift
+++ Tests/Days/07.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day7a = ""
-    @objc static let day7b = day7a
+    @objc static let day7a = "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags."
+    @objc static let day7b = "shiny gold bags contain 2 dark red bags.\ndark red bags contain 2 dark orange bags.\ndark orange bags contain 2 dark yellow bags.\ndark yellow bags contain 2 dark green bags.\ndark green bags contain 2 dark blue bags.\ndark blue bags contain 2 dark violet bags.\ndark violet bags contain no other bags."
 }
 
 extension Outputs {
-    @objc static let day7a = "0"
-    @objc static let day7b = "0"
+    @objc static let day7a = "4"
+    @objc static let day7b = "126"
 }
