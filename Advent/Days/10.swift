35284179b10c4ab5e66fee1b98f2c769caf89aac
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-10 10:31:58 +0100

    Day 10

--- Advent/Days/10.swift
+++ Advent/Days/10.swift
@@ -9,12 +9,51 @@ import Foundation
 
 extension Solvers {
     @objc static let day10a: Solve = { input in
-
-        return "0"
+        let values = processInput(input)
+        let groups = createGroups(values)
+        let diff3 = groups.count
+        let diff1 = groups.reduce(0, { $0 + $1.count - 1 })
+        return "\(diff1 * diff3)"
     }
     
     @objc static let day10b: Solve = { input in
-
-        return "0"
+        let values = processInput(input)
+        let groups = createGroups(values)
+        let paths = calculatePaths(groups)
+        return "\(paths)"
     }
+}
+
+fileprivate func processInput(_ string: String) -> [Int] {
+    let lines = string.components(separatedBy: "\n")
+    return [0] + lines.compactMap { Int($0) }
+}
+
+fileprivate func createGroups(_ input: [Int]) -> [[Int]] {
+    var groups: [[Int]] = []
+    var group: [Int] = []
+    var previous = -1
+    for value in input.sorted() {
+        if value != previous + 1 {
+            groups.append(group)
+            group = []
+        }
+        group.append(value)
+        previous = value
+    }
+    groups += [group]
+    return groups
+}
+
+fileprivate func calculatePaths(_ groups: [[Int]]) -> Int {
+    var paths = 1
+    for group in groups {
+        switch group.count {
+        case 3: paths *= 2
+        case 4: paths *= 4
+        case 5: paths *= 7
+        default: break
+        }
+    }
+    return paths
 }
--- Tests/Days/10.swift
+++ Tests/Days/10.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day10a = ""
+    @objc static let day10a = "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
     @objc static let day10b = day10a
 }
 
 extension Outputs {
-    @objc static let day10a = "0"
-    @objc static let day10b = "0"
+    @objc static let day10a = "220"
+    @objc static let day10b = "19208"
 }
