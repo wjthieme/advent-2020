e72aacdbf46b30e75bc3e3bada12bdfbe8d134a1
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-13 19:36:38 +0100

    Day 13

--- Advent/Days/13.swift
+++ Advent/Days/13.swift
@@ -9,12 +9,40 @@ import Foundation
 
 extension Solvers {
     @objc static let day13a: Solve = { input in
-
+        guard let (timestamp, busses) = processInput(input) else { return "0" }
+        for n in 0..<1000 {
+            for bus in busses {
+                if bus == -1 { continue }
+                if (timestamp + n) % bus == 0 {
+                    return "\(n * bus)"
+                }
+            }
+        }
         return "0"
     }
     
     @objc static let day13b: Solve = { input in
-
-        return "0"
+        guard let (_, busses) = processInput(input) else { return "0" }
+        var counter = 0
+        var factor = 1
+        
+        for (offset, value) in busses.enumerated() {
+            if value == -1 { continue }
+            while (counter + offset) % value != 0 {
+                counter += factor
+            }
+            factor *= value
+        }
+        
+        return "\(counter)"
     }
 }
+
+fileprivate func processInput(_ string: String) -> (Int, [Int])? {
+    let lines = string.components(separatedBy: "\n")
+    guard let int = Int(lines[0]) else { return nil }
+    let ids = lines[1].components(separatedBy: ",").compactMap { Int($0.replacingOccurrences(of: "x", with: "-1")) }
+    return (int, ids)
+}
+
+
--- Tests/Days/13.swift
+++ Tests/Days/13.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day13a = ""
-    @objc static let day13b = day13a
+    @objc static let day13a = "939\n7,13,x,x,59,x,31,19"
+    @objc static let day13b = "0\n1789,37,47,1889"
 }
 
 extension Outputs {
-    @objc static let day13a = "0"
-    @objc static let day13b = "0"
+    @objc static let day13a = "295"
+    @objc static let day13b = "1202161486"
 }
