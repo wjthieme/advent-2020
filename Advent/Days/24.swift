0176afe2cb8bae3e7aaf270b57cbee0f29d79ee2
Wilhelm Thieme <will.thieme@sogeti.com>
2020-12-24 10:27:34 +0100

    Day 24

--- Advent/Days/24.swift
+++ Advent/Days/24.swift
@@ -9,12 +9,122 @@ import Foundation
 
 extension Solvers {
     @objc static let day24a: Solve = { input in
-
-        return "0"
+        let values = processInput(input)
+        var result: Set<Tile> = []
+        values.forEach({ result.toggle($0) })
+        return "\(result.count)"
     }
     
     @objc static let day24b: Solve = { input in
-
-        return "0"
+        let values = processInput(input)
+        var result: Set<Tile> = []
+        values.forEach({ result.toggle($0) })
+        guard let minX = values.min(by: { $0.x < $1.x })?.x else { return "0" }
+        guard let maxX = values.max(by: { $0.x < $1.x })?.x else { return "0" }
+        guard let minY = values.min(by: { $0.y < $1.y })?.y else { return "0" }
+        guard let maxY = values.max(by: { $0.y < $1.y })?.y else { return "0" }
+        var xRange = minX-1...maxX+1
+        var yRange = minY-1...maxY+1
+        
+        for _ in 0..<100 {
+            result = convolve(result, xRange: xRange, yRange: yRange)
+            xRange = xRange.expanded(by: 1)
+            yRange = yRange.expanded(by: 1)
+        }
+        
+        return "\(result.count)"
     }
+}
+
+fileprivate func processInput(_ string: String) -> [Tile] {
+    let lines = string.components(separatedBy: "\n")
+    return lines.map({ Tile($0) })
+}
+
+fileprivate class Tile: Hashable {
+    static func == (lhs: Tile, rhs: Tile) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+    
+    let x: Int
+    let y: Int
+    
+    convenience init(_ string: String) {
+        var remaining = string
+        var x = 0
+        var y = 0
+        
+        while !remaining.isEmpty {
+            var getSecond = false
+            let first = remaining.removeFirst()
+            switch first {
+            case "n": y += 1; getSecond = true
+            case "e": x += 2
+            case "w": x -= 2
+            case "s": y -= 1; getSecond = true
+            default: break
+            }
+            
+            if getSecond {
+                let second = remaining.removeFirst()
+                switch second {
+                case "e": x += 1
+                case "w": x -= 1
+                default: break
+                }
+            }
+        }
+        
+        self.init(x: x, y: y)
+    }
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
+}
+
+fileprivate extension Set {
+    mutating func toggle(_ member: Element) {
+        if contains(member) {
+            remove(member)
+        } else {
+            insert(member)
+        }
+    }
+}
+
+fileprivate extension ClosedRange where Bound == Int {
+    
+    func expanded(by value: Bound) -> ClosedRange {
+        let lower = lowerBound - value
+        let upper = upperBound + value
+        return lower...upper
+    }
+}
+
+fileprivate let kernel = [ (-1, -1), (-2, 0), (-1, 1), (1, -1), (2, 0), (1, 1) ]
+
+fileprivate func convolve(_ input: Set<Tile>, xRange: ClosedRange<Int>, yRange: ClosedRange<Int>) -> Set<Tile> {
+    var result = input
+    for x in xRange {
+        for y in yRange {
+            if abs(y % 2) != abs(x % 2) { continue }
+            let own = Tile(x: x, y: y)
+            let adjecent = kernel.map({ ($0.0 + x, $0.1 + y) })
+            let contains = adjecent.map({ input.contains(Tile(x: $0.0, y: $0.1))  })
+            let count = contains.filter({ $0 }).count
+            if input.contains(own) && (count == 0 || count > 2) {
+                result.remove(own)
+            } else if !input.contains(own) && count == 2 {
+                result.insert(own)
+            }
+        }
+    }
+    return result
 }
--- Tests/Days/24.swift
+++ Tests/Days/24.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension Inputs {
-    @objc static let day24a = ""
+    @objc static let day24a = "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew"
     @objc static let day24b = day24a
 }
 
 extension Outputs {
-    @objc static let day24a = "0"
-    @objc static let day24b = "0"
+    @objc static let day24a = "10"
+    @objc static let day24b = "2208"
 }
